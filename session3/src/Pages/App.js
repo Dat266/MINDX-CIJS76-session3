cb3246b11f584c04f6deffcf2e8699be039c4092
Dat266 <datdev266@gmail.com>
2022-09-12 08:52:54 +0700

    add file session3

--- session3/src/Pages/App.js
+++ session3/src/Pages/App.js
@@ -1,51 +1,41 @@
-import Input from "../components/Input";
+import { useState } from "react";
+import React from "react";
 import "./App.css";
 
 function App() {
+	const [color, setColor] = useState(1);
+
+	const listColor = [
+		{
+			id: 1,
+			color: "blue",
+		},
+		{
+			id: 2,
+			color: "red",
+		},
+		{
+			id: 3,
+			color: "orange",
+		},
+	];
+
+	const handleClick = () => {
+		setColor((prev) => (prev === 3 ? 1 : prev + 1));
+	};
+
 	return (
-		<div className="container">
-			<form>
-				<div>
-					<Input
-						type="text"
-						placeholder="Enter your task here ..."
-						className="main-input"
-					/>
-				</div>
-				<div>
-					<div className="item">
-						<Input
-							type="radio"
-							labelName="Clean up bedroom"
-							className="check-input"
-						/>
-					</div>
-					<div className="item">
-						<Input
-							type="radio"
-							labelName="Buy some milk"
-							className="check-input"
-						/>
-					</div>
-					<div className="item">
-						<Input type="radio" labelName="Jogging" className="check-input" />
-					</div>
-					<div className="item">
-						<Input
-							type="radio"
-							labelName="Learn React"
-							className="check-input"
-						/>
-					</div>
-					<div className="item">
-						<Input
-							type="radio"
-							labelName="Doing Excercises"
-							className="check-input"
-						/>
-					</div>
-				</div>
-			</form>
+		<div className="main">
+			<button className="btn" onClick={handleClick}>
+				NEXT
+			</button>
+			<div className="content">
+				{listColor.map((item) => {
+					return (
+						<div className="item" id={item.id == color ? item.color : ""}></div>
+					);
+				})}
+			</div>
 		</div>
 	);
 }
